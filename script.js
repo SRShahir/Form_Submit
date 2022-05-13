3fed10a2ccebb962fa43c13c5af06c9ed2ae97e1
SRShahir <shreyashahir20@gmail.com>
2022-05-13 18:09:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+console.log("asdf")
+function submitData(e) {
+    console.log("fgh");
+
+    let x = {
+        title: document.getElementById("title").value,
+        discription: document.getElementById("desc").value,
+        date: document.getElementById("date").value
+    }
+
+    console.log(x);
+
+
+    // document.getElementById("title").value = "";
+
+}
